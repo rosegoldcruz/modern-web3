dd1df3da9965a7f90ffdd3765468249c6e33f9e6
rosegoldcruz <vulpine444@gmail.com>
2026-06-05 19:19:39 -0700

    feat: grant member access from Stripe payments

--- app/learn/(vault)/academy/page.tsx
+++ app/learn/(vault)/academy/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'
+
 export default function AcademyPage() {
   redirect('https://member.ironvaulttoken.com/dashboard')
 }
